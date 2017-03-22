c64c462e572fbe54797aba7500a474f79c57f3a4
Dmitry-Ship <vooxil@gmail.com>
2017-03-22 22:56:42 +0300

    responsive welcome page

--- middleware/frontEndMiddleware.js
+++ middleware/frontEndMiddleware.js
@@ -3,7 +3,6 @@ const path = require('path');
 const express = require('express');
 
 module.exports = (app) => {
-  console.log(`NODE_ENV: ${process.env.NODE_ENV} production, message: ${process.env.NODE_ENV ? 'hey' : 'bye' }`);
   if (isDeveloping) {
     const webpack = require('webpack');
     const webpackMiddleware = require('webpack-dev-middleware');
--- src/components/pages/WelcomePage/WelcomePage.jsx
+++ src/components/pages/WelcomePage/WelcomePage.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { page, heroTitle, note, noteText, heroSub } from './WelcomePage.scss';
+import { page, heroTitle, note, noteText, heroSub, wrapper } from './WelcomePage.scss';
 import GetStartedContainer from '../../../containers/GetStartedContainer';
 
 const WelcomePage = () => (
   <div className={page}>
-    <h1 className={heroTitle} >One place to store all your ideas.</h1>
-    <div className={note} >
-      <span className={noteText} >Just</span>
-      <span className={noteText} >take</span>
-      <span className={noteText} >a</span>
-      <span className={noteText} >note</span>
+    <div className={wrapper} >
+
+      <h1 className={heroTitle} >One place to store all your ideas.</h1>
+      <div className={note} >
+        <span className={noteText} >Just</span>
+        <span className={noteText} >take</span>
+        <span className={noteText} >a</span>
+        <span className={noteText} >note</span>
+      </div>
+      <h2 className={heroSub} >Cloverfield is an advanced todo app.</h2>
     </div>
-    <h1 className={heroSub} >Cloverfield is an advanced todo app.</h1>
     
     <GetStartedContainer />
   </div>
