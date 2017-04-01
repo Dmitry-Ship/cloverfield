744b8e4073af837b964f4a4be37ca555994568ba
Dmitry-Ship <vooxil@gmail.com>
2017-04-01 14:14:46 +0300

    removed Game component

--- src/components/pages/AboutPage/AboutPage.jsx
+++ src/components/pages/AboutPage/AboutPage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-
 import TechCard from '../../TechCard';
-import Game from '../../Game';
 import { page } from './AboutPage.scss';
 
 const AboutPage = () => (
   <div className={page} >
     <TechCard />
-    <Game />
+
   </div>
 );
 
