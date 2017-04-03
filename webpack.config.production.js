a7fc30f106bbced871f38766f13982d4643a7439
Dmitry-Ship <vooxil@gmail.com>
2017-04-04 01:54:14 +0300

    heroku fix

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -6,13 +6,11 @@ const lost = require('lost');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   devtool: 'cheap-module-source-map',
-  // entry: './src/index',
   entry: {
-    main: './src/index',
+    app: './src/index',
     vendor: [
       'react',
       'react-router-dom',
@@ -25,14 +23,11 @@ module.exports = {
       'react-textarea-autosize',
       'redux-logger',
       'react-hot-loader',
-    ]
+    ],
   },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: '[name].js',
-    
-    // filename: '[name].[chunkhash].js',
-    // chunkFilename: '[name].[chunkhash].js',
+    filename: '[name].bundle.js',
     publicPath: '/',
   },
   resolve: {
@@ -81,45 +76,14 @@ module.exports = {
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      // minChunks: function (module) {
-      //   return module.context && module.context.indexOf('node_modules') !== -1;
-      // },
     }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'manifest',
-    //   minChunks: Infinity,
-    // }),
-
-    // new CompressionPlugin({
-    //   asset: "[path].gz[query]",
-    //   algorithm: "gzip",
-    //   test: /\.(js|html)$/,
-    //   threshold: 10240,
-    //   minRatio: 0.8
-    // }),
-
-    new webpack.HashedModuleIdsPlugin(),
     new ExtractTextPlugin({
-      // filename: 'style.[contenthash].css',
-      filename: 'style.css',
-      
+      filename: '[name].style.css',
       ignoreOrder: true,
       allChunks: true,
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      // minify: {
-      //   removeComments: true,
-      //   collapseWhitespace: true,
-      //   removeRedundantAttributes: true,
-      //   useShortDoctype: true,
-      //   removeEmptyAttributes: true,
-      //   removeStyleLinkTypeAttributes: true,
-      //   keepClosingSlash: true,
-      //   minifyJS: true,
-      //   minifyCSS: true,
-      //   minifyURLs: true,
-      // },
       template: './index.html',
     }),
   ],
