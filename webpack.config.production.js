9abda575209560878e58bf3ddd52a6b2bea9abcd
Dmitry-Ship <vooxil@gmail.com>
2017-04-04 01:46:35 +0300

    heroku fix

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -10,23 +10,23 @@ const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   devtool: 'cheap-module-source-map',
-  entry: './src/index',
-  // entry: {
-  //   main: './src/index',
-  //   vendor: [
-  //     'react',
-  //     'react-router-dom',
-  //     'react-redux',
-  //     'redux',
-  //     'redux-thunk',
-  //     'axios',
-  //     'react-dom',
-  //     'react-masonry-component',
-  //     'react-textarea-autosize',
-  //     'redux-logger',
-  //     'react-hot-loader',
-  //   ]
-  // },
+  // entry: './src/index',
+  entry: {
+    main: './src/index',
+    vendor: [
+      'react',
+      'react-router-dom',
+      'react-redux',
+      'redux',
+      'redux-thunk',
+      'axios',
+      'react-dom',
+      'react-masonry-component',
+      'react-textarea-autosize',
+      'redux-logger',
+      'react-hot-loader',
+    ]
+  },
   output: {
     path: path.join(__dirname, 'build'),
     filename: '[name].[chunkhash].js',
@@ -72,23 +72,16 @@ module.exports = {
           values,
           lost,
           colorFunction,
-          autoprefixer({
-            browsers: [
-              '>1%',
-              'last 4 versions',
-              'Firefox ESR',
-              'not ie < 9',
-            ],
-          }),
+          autoprefixer,
         ],
       },
     }),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      minChunks: function (module) {
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      },
+      // minChunks: function (module) {
+      //   return module.context && module.context.indexOf('node_modules') !== -1;
+      // },
     }),
     // new webpack.optimize.CommonsChunkPlugin({
     //   name: 'manifest',
@@ -111,18 +104,18 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
+      // minify: {
+      //   removeComments: true,
+      //   collapseWhitespace: true,
+      //   removeRedundantAttributes: true,
+      //   useShortDoctype: true,
+      //   removeEmptyAttributes: true,
+      //   removeStyleLinkTypeAttributes: true,
+      //   keepClosingSlash: true,
+      //   minifyJS: true,
+      //   minifyCSS: true,
+      //   minifyURLs: true,
+      // },
       template: './index.html',
     }),
   ],
